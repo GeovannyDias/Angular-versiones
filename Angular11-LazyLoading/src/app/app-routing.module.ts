c4f9fc5cd6248cbb9fd09ec8b18cd0ea8c831120
GeovannyDias <geova_jd@hotmail.com>
2020-12-10 23:59:57 -0500

    Lazy Loading

--- Angular11-LazyLoading/src/app/app-routing.module.ts
+++ Angular11-LazyLoading/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: () => import('./components/customers/customers.module').then(m => m.CustomersModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./components/orders/orders.module').then(m => m.OrdersModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
